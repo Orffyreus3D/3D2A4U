8395448672614278931758fa48df0c96d32dc5be
Orffyreus3D <destroymassmedia@gmail.com>
2024-08-15 21:47:21 -0600

    Sorting of EditLookupValues on load, and button to do sort

--- 3D2A4U_Manager/EditLookupValues.Designer.cs
+++ 3D2A4U_Manager/EditLookupValues.Designer.cs
@@ -20,212 +20,224 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            tblStats = new TableLayoutPanel();
-            label3 = new Label();
-            txtName = new TextBox();
-            label2 = new Label();
-            txtID = new TextBox();
-            nudSortOrder = new NumericUpDown();
-            label1 = new Label();
-            btnCommit = new Button();
-            lstValues = new ListBox();
-            btnSave = new Button();
-            btnCancel = new Button();
-            btnAddValue = new Button();
-            btnRemoveValue = new Button();
-            btnSortUp = new Button();
-            btnSortDown = new Button();
-            tblStats.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)nudSortOrder).BeginInit();
-            SuspendLayout();
-            // 
-            // tblStats
-            // 
-            tblStats.ColumnCount = 2;
-            tblStats.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.93296F));
-            tblStats.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 72.06704F));
-            tblStats.Controls.Add(label3, 0, 2);
-            tblStats.Controls.Add(txtName, 1, 1);
-            tblStats.Controls.Add(label2, 0, 1);
-            tblStats.Controls.Add(txtID, 1, 0);
-            tblStats.Controls.Add(nudSortOrder, 1, 2);
-            tblStats.Controls.Add(label1, 0, 0);
-            tblStats.Controls.Add(btnCommit, 1, 3);
-            tblStats.Location = new Point(251, 12);
-            tblStats.Name = "tblStats";
-            tblStats.RowCount = 4;
-            tblStats.RowStyles.Add(new RowStyle());
-            tblStats.RowStyles.Add(new RowStyle());
-            tblStats.RowStyles.Add(new RowStyle());
-            tblStats.RowStyles.Add(new RowStyle());
-            tblStats.Size = new Size(537, 113);
-            tblStats.TabIndex = 0;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(3, 58);
-            label3.Name = "label3";
-            label3.Size = new Size(61, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Sort Order";
-            // 
-            // txtName
-            // 
-            txtName.Location = new Point(153, 32);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(381, 23);
-            txtName.TabIndex = 3;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(3, 29);
-            label2.Name = "label2";
-            label2.Size = new Size(39, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Name";
-            // 
-            // txtID
-            // 
-            txtID.Enabled = false;
-            txtID.Location = new Point(153, 3);
-            txtID.Name = "txtID";
-            txtID.Size = new Size(120, 23);
-            txtID.TabIndex = 1;
-            // 
-            // nudSortOrder
-            // 
-            nudSortOrder.Location = new Point(153, 61);
-            nudSortOrder.Name = "nudSortOrder";
-            nudSortOrder.Size = new Size(120, 23);
-            nudSortOrder.TabIndex = 5;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(3, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(18, 15);
-            label1.TabIndex = 0;
-            label1.Text = "ID";
-            // 
-            // btnCommit
-            // 
-            btnCommit.Location = new Point(153, 90);
-            btnCommit.Name = "btnCommit";
-            btnCommit.Size = new Size(75, 23);
-            btnCommit.TabIndex = 6;
-            btnCommit.Text = "Commit";
-            btnCommit.UseVisualStyleBackColor = true;
-            btnCommit.Click += btnCommit_Click;
-            // 
-            // lstValues
-            // 
-            lstValues.DisplayMember = "name";
-            lstValues.FormattingEnabled = true;
-            lstValues.ItemHeight = 15;
-            lstValues.Location = new Point(12, 12);
-            lstValues.Name = "lstValues";
-            lstValues.Size = new Size(233, 394);
-            lstValues.TabIndex = 0;
-            lstValues.ValueMember = "id";
-            lstValues.SelectedIndexChanged += lstValues_SelectedIndexChanged;
-            // 
-            // btnSave
-            // 
-            btnSave.DialogResult = DialogResult.OK;
-            btnSave.Location = new Point(632, 409);
-            btnSave.Name = "btnSave";
-            btnSave.Size = new Size(75, 23);
-            btnSave.TabIndex = 1;
-            btnSave.Text = "Save";
-            btnSave.UseVisualStyleBackColor = true;
-            btnSave.Click += btnSave_Click;
-            // 
-            // btnCancel
-            // 
-            btnCancel.DialogResult = DialogResult.Cancel;
-            btnCancel.Location = new Point(713, 409);
-            btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(75, 23);
-            btnCancel.TabIndex = 2;
-            btnCancel.Text = "Cancel";
-            btnCancel.UseVisualStyleBackColor = true;
-            btnCancel.Click += btnCancel_Click;
-            // 
-            // btnAddValue
-            // 
-            btnAddValue.Location = new Point(12, 409);
-            btnAddValue.Name = "btnAddValue";
-            btnAddValue.Size = new Size(75, 23);
-            btnAddValue.TabIndex = 3;
-            btnAddValue.Text = "Add";
-            btnAddValue.UseVisualStyleBackColor = true;
-            btnAddValue.Click += btnAddValue_Click;
-            // 
-            // btnRemoveValue
-            // 
-            btnRemoveValue.Location = new Point(93, 409);
-            btnRemoveValue.Name = "btnRemoveValue";
-            btnRemoveValue.Size = new Size(75, 23);
-            btnRemoveValue.TabIndex = 4;
-            btnRemoveValue.Text = "Remove";
-            btnRemoveValue.UseVisualStyleBackColor = true;
-            btnRemoveValue.Click += btnRemoveValue_click;
-            // 
-            // btnSortUp
-            // 
-            btnSortUp.Location = new Point(174, 409);
-            btnSortUp.Name = "btnSortUp";
-            btnSortUp.Size = new Size(32, 23);
-            btnSortUp.TabIndex = 5;
-            btnSortUp.Text = "↑";
-            btnSortUp.UseVisualStyleBackColor = true;
-            // 
-            // btnSortDown
-            // 
-            btnSortDown.Location = new Point(212, 409);
-            btnSortDown.Name = "btnSortDown";
-            btnSortDown.Size = new Size(32, 23);
-            btnSortDown.TabIndex = 6;
-            btnSortDown.Text = "↓";
-            btnSortDown.UseVisualStyleBackColor = true;
-            // 
-            // EditLookupValues
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 444);
-            Controls.Add(btnSortDown);
-            Controls.Add(btnSortUp);
-            Controls.Add(btnRemoveValue);
-            Controls.Add(btnAddValue);
-            Controls.Add(btnCancel);
-            Controls.Add(btnSave);
-            Controls.Add(lstValues);
-            Controls.Add(tblStats);
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            Name = "EditLookupValues";
-            Text = "EditLookupValues";
-            Load += EditLookupValues_Load;
-            tblStats.ResumeLayout(false);
-            tblStats.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)nudSortOrder).EndInit();
-            ResumeLayout(false);
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			tblStats = new TableLayoutPanel();
+			label3 = new Label();
+			txtName = new TextBox();
+			label2 = new Label();
+			txtID = new TextBox();
+			nudSortOrder = new NumericUpDown();
+			label1 = new Label();
+			btnCommit = new Button();
+			lstValues = new ListBox();
+			btnSave = new Button();
+			btnCancel = new Button();
+			btnAddValue = new Button();
+			btnRemoveValue = new Button();
+			btnSortUp = new Button();
+			btnSortDown = new Button();
+			btnSort = new Button();
+			tblStats.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)nudSortOrder).BeginInit();
+			SuspendLayout();
+			// 
+			// tblStats
+			// 
+			tblStats.ColumnCount = 2;
+			tblStats.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.93296F));
+			tblStats.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 72.06704F));
+			tblStats.Controls.Add(label3, 0, 2);
+			tblStats.Controls.Add(txtName, 1, 1);
+			tblStats.Controls.Add(label2, 0, 1);
+			tblStats.Controls.Add(txtID, 1, 0);
+			tblStats.Controls.Add(nudSortOrder, 1, 2);
+			tblStats.Controls.Add(label1, 0, 0);
+			tblStats.Controls.Add(btnCommit, 1, 3);
+			tblStats.Location = new Point(251, 12);
+			tblStats.Name = "tblStats";
+			tblStats.RowCount = 4;
+			tblStats.RowStyles.Add(new RowStyle());
+			tblStats.RowStyles.Add(new RowStyle());
+			tblStats.RowStyles.Add(new RowStyle());
+			tblStats.RowStyles.Add(new RowStyle());
+			tblStats.Size = new Size(537, 113);
+			tblStats.TabIndex = 0;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(3, 58);
+			label3.Name = "label3";
+			label3.Size = new Size(61, 15);
+			label3.TabIndex = 4;
+			label3.Text = "Sort Order";
+			// 
+			// txtName
+			// 
+			txtName.Location = new Point(153, 32);
+			txtName.Name = "txtName";
+			txtName.Size = new Size(381, 23);
+			txtName.TabIndex = 3;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(3, 29);
+			label2.Name = "label2";
+			label2.Size = new Size(39, 15);
+			label2.TabIndex = 2;
+			label2.Text = "Name";
+			// 
+			// txtID
+			// 
+			txtID.Enabled = false;
+			txtID.Location = new Point(153, 3);
+			txtID.Name = "txtID";
+			txtID.Size = new Size(120, 23);
+			txtID.TabIndex = 1;
+			// 
+			// nudSortOrder
+			// 
+			nudSortOrder.Location = new Point(153, 61);
+			nudSortOrder.Name = "nudSortOrder";
+			nudSortOrder.Size = new Size(120, 23);
+			nudSortOrder.TabIndex = 5;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(3, 0);
+			label1.Name = "label1";
+			label1.Size = new Size(18, 15);
+			label1.TabIndex = 0;
+			label1.Text = "ID";
+			// 
+			// btnCommit
+			// 
+			btnCommit.Location = new Point(153, 90);
+			btnCommit.Name = "btnCommit";
+			btnCommit.Size = new Size(75, 23);
+			btnCommit.TabIndex = 6;
+			btnCommit.Text = "Commit";
+			btnCommit.UseVisualStyleBackColor = true;
+			btnCommit.Click += btnCommit_Click;
+			// 
+			// lstValues
+			// 
+			lstValues.DisplayMember = "name";
+			lstValues.FormattingEnabled = true;
+			lstValues.ItemHeight = 15;
+			lstValues.Location = new Point(12, 12);
+			lstValues.Name = "lstValues";
+			lstValues.Size = new Size(233, 394);
+			lstValues.TabIndex = 0;
+			lstValues.ValueMember = "id";
+			lstValues.SelectedIndexChanged += lstValues_SelectedIndexChanged;
+			// 
+			// btnSave
+			// 
+			btnSave.DialogResult = DialogResult.OK;
+			btnSave.Location = new Point(632, 409);
+			btnSave.Name = "btnSave";
+			btnSave.Size = new Size(75, 23);
+			btnSave.TabIndex = 1;
+			btnSave.Text = "Save";
+			btnSave.UseVisualStyleBackColor = true;
+			btnSave.Click += btnSave_Click;
+			// 
+			// btnCancel
+			// 
+			btnCancel.DialogResult = DialogResult.Cancel;
+			btnCancel.Location = new Point(713, 409);
+			btnCancel.Name = "btnCancel";
+			btnCancel.Size = new Size(75, 23);
+			btnCancel.TabIndex = 2;
+			btnCancel.Text = "Cancel";
+			btnCancel.UseVisualStyleBackColor = true;
+			btnCancel.Click += btnCancel_Click;
+			// 
+			// btnAddValue
+			// 
+			btnAddValue.Location = new Point(12, 409);
+			btnAddValue.Name = "btnAddValue";
+			btnAddValue.Size = new Size(46, 23);
+			btnAddValue.TabIndex = 3;
+			btnAddValue.Text = "Add";
+			btnAddValue.UseVisualStyleBackColor = true;
+			btnAddValue.Click += btnAddValue_Click;
+			// 
+			// btnRemoveValue
+			// 
+			btnRemoveValue.Location = new Point(62, 409);
+			btnRemoveValue.Name = "btnRemoveValue";
+			btnRemoveValue.Size = new Size(59, 23);
+			btnRemoveValue.TabIndex = 4;
+			btnRemoveValue.Text = "Remove";
+			btnRemoveValue.UseVisualStyleBackColor = true;
+			btnRemoveValue.Click += btnRemoveValue_click;
+			// 
+			// btnSortUp
+			// 
+			btnSortUp.Location = new Point(175, 409);
+			btnSortUp.Name = "btnSortUp";
+			btnSortUp.Size = new Size(32, 23);
+			btnSortUp.TabIndex = 5;
+			btnSortUp.Text = "↑";
+			btnSortUp.UseVisualStyleBackColor = true;
+			// 
+			// btnSortDown
+			// 
+			btnSortDown.Location = new Point(211, 409);
+			btnSortDown.Name = "btnSortDown";
+			btnSortDown.Size = new Size(32, 23);
+			btnSortDown.TabIndex = 6;
+			btnSortDown.Text = "↓";
+			btnSortDown.UseVisualStyleBackColor = true;
+			// 
+			// btnSort
+			// 
+			btnSort.Location = new Point(125, 409);
+			btnSort.Name = "btnSort";
+			btnSort.Size = new Size(46, 23);
+			btnSort.TabIndex = 7;
+			btnSort.Text = "Sort";
+			btnSort.UseVisualStyleBackColor = true;
+			btnSort.Click += btnSort_Click;
+			// 
+			// EditLookupValues
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 444);
+			Controls.Add(btnSort);
+			Controls.Add(btnSortDown);
+			Controls.Add(btnSortUp);
+			Controls.Add(btnRemoveValue);
+			Controls.Add(btnAddValue);
+			Controls.Add(btnCancel);
+			Controls.Add(btnSave);
+			Controls.Add(lstValues);
+			Controls.Add(tblStats);
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+			Name = "EditLookupValues";
+			Text = "EditLookupValues";
+			Load += EditLookupValues_Load;
+			tblStats.ResumeLayout(false);
+			tblStats.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)nudSortOrder).EndInit();
+			ResumeLayout(false);
+		}
 
-        #endregion
+		#endregion
 
-        private TableLayoutPanel tblStats;
+		private TableLayoutPanel tblStats;
         private ListBox lstValues;
         private Button btnSave;
         private Button btnCancel;
@@ -240,5 +252,6 @@
         private NumericUpDown nudSortOrder;
         private Label label1;
         private Button btnCommit;
-    }
+		private Button btnSort;
+	}
 }
--- 3D2A4U_Manager/EditLookupValues.cs
+++ 3D2A4U_Manager/EditLookupValues.cs
@@ -16,95 +16,111 @@ using System.Windows.Forms;
 
 namespace _3D2A4U_Manager
 {
-    public partial class EditLookupValues : Form
-    {
-        private LookupValue? SelectedItem { get { return (LookupValue?)lstValues?.SelectedItem; } set { lstValues.SelectedItem = value; } }
-        public Type ValueType { get; set; } = typeof(string);
-        private readonly _3D2A4UBusinessLayer biz = new(Settings.Default.LocalPath);
-        private readonly VirtualDatabase vdb = new(Settings.Default.LocalPath);
+	public partial class EditLookupValues : Form
+	{
+		private LookupValue? SelectedItem { get { return (LookupValue?)lstValues?.SelectedItem; } set { lstValues.SelectedItem = value; } }
+		public Type ValueType { get; set; } = typeof(string);
+		private readonly _3D2A4UBusinessLayer biz = new(Settings.Default.LocalPath);
+		private readonly VirtualDatabase vdb = new(Settings.Default.LocalPath);
 
-        public EditLookupValues(Type valueType)
-        {
-            InitializeComponent();
-            ValueType = valueType;
-        }
+		public EditLookupValues(Type valueType)
+		{
+			InitializeComponent();
+			ValueType = valueType;
+		}
 
-        public EditLookupValues(string valueTypeName) : this(Assembly.GetAssembly(typeof(LookupValue))?.GetType("_3D2A4U_Model." + valueTypeName) ?? typeof(object)) { }
+		public EditLookupValues(string valueTypeName) : this(Assembly.GetAssembly(typeof(LookupValue))?.GetType("_3D2A4U_Model." + valueTypeName) ?? typeof(object)) { }
 
-        private void btnRemoveValue_click(object sender, EventArgs e)
-        {
-            if (lstValues.SelectedItem != null)
-                lstValues.Items.Remove(lstValues.SelectedItem);
-        }
+		private void btnRemoveValue_click(object sender, EventArgs e)
+		{
+			if (lstValues.SelectedItem != null)
+				lstValues.Items.Remove(lstValues.SelectedItem);
+		}
 
-        private void btnAddValue_Click(object sender, EventArgs e)
-        {
-            txtID.Clear();
-            txtName.Clear();
-            nudSortOrder.Value = 0;
-            lstValues.SelectedIndex = -1;
-        }
+		private void btnAddValue_Click(object sender, EventArgs e)
+		{
+			txtID.Clear();
+			txtName.Clear();
+			nudSortOrder.Value = 0;
+			lstValues.SelectedIndex = -1;
+		}
 
-        private void btnCommit_Click(object sender, EventArgs e)
-        {
-            if (SelectedItem != null)
-            {
-                //just update this selected item, all done for now
-                SelectedItem.Id = int.Parse(txtID.Text);
-                SelectedItem.Name = txtName.Text;
-                SelectedItem.SortOrder = (int)nudSortOrder.Value;
-            }
-            else
-            {
-                //create a new instance of our specific LookupValue subtype
-                var newVal = Activator.CreateInstance(ValueType, [txtName.Text, (int)nudSortOrder.Value]);
-                if (newVal != null)
-                    ((System.Collections.IList)lstValues.DataSource).Add(newVal);
-                //refresh the control so it shows the new guy
-                var tmp = lstValues.DataSource;
-                lstValues.DataSource = new List<LookupValue>(); //throw a dummy list in here so we don't lose our Display- and ValueMembers
-                lstValues.DataSource = tmp;
-            }
-        }
+		private void btnCommit_Click(object sender, EventArgs e)
+		{
+			if (SelectedItem != null)
+			{
+				//just update this selected item, all done for now
+				SelectedItem.Id = int.Parse(txtID.Text);
+				SelectedItem.Name = txtName.Text;
+				SelectedItem.SortOrder = (int)nudSortOrder.Value;
+			}
+			else
+			{
+				//create a new instance of our specific LookupValue subtype
+				var newVal = Activator.CreateInstance(ValueType, [txtName.Text, (int)nudSortOrder.Value]);
+				if (newVal != null)
+					((System.Collections.IList)lstValues.DataSource).Add(newVal);
+				//refresh the control so it shows the new guy
+				var tmp = lstValues.DataSource;
+				lstValues.DataSource = new List<LookupValue>(); //throw a dummy list in here so we don't lose our Display- and ValueMembers
+				lstValues.DataSource = tmp;
+			}
+		}
 
-        /// <summary>
-        /// Populate the list with the existing values for this type
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void EditLookupValues_Load(object sender, EventArgs e)
-        {
-            //lstValues.DataSource = biz.GetLookupValues(ValueType.Name);
-            //foreach (var item in biz.GetLookupValues(ValueType.Name))
-            //    lstValues.Items.Add(item);
+		/// <summary>
+		/// Populate the list with the existing values for this type
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void EditLookupValues_Load(object sender, EventArgs e)
+		{
+			//lstValues.DataSource = biz.GetLookupValues(ValueType.Name);
+			//foreach (var item in biz.GetLookupValues(ValueType.Name))
+			//    lstValues.Items.Add(item);
 
-            //var bob = typeof(VirtualDatabase).GetProperty(ValueType.Name + "s")?.GetValue(vdb);
-            lstValues.DataSource = vdb.GetList(ValueType);
-        }
+			//var bob = typeof(VirtualDatabase).GetProperty(ValueType.Name + "s")?.GetValue(vdb);
+			lstValues.DataSource = ((IEnumerable<LookupValue>)vdb.GetList(ValueType)).OrderBy(lv => lv.SortOrder).ToList();
+		}
 
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            foreach (var item in (IList)lstValues.DataSource)
-            {
-                biz.Save((LookupValue)item);
-            }
-        }
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			foreach (var item in (IList)lstValues.DataSource)
+			{
+				biz.Save((LookupValue)item);
+			}
+			lstValues.DataSource = vdb.GetList(ValueType); //reload our list
+		}
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
-        }
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+			this.Close();
+		}
 
-        private void lstValues_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            LookupValue si = (LookupValue)lstValues.SelectedItem;
-            if (si != null)
-            {
-                txtID.Text = si.Id.ToString();
-                txtName.Text = si.Name;
-                nudSortOrder.Value = si.SortOrder;
-            }
-        }
-    }
+		private void lstValues_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			LookupValue si = (LookupValue)lstValues.SelectedItem;
+			if (si != null)
+			{
+				txtID.Text = si.Id.ToString();
+				txtName.Text = si.Name;
+				nudSortOrder.Value = si.SortOrder;
+			}
+		}
+
+		/// <summary>
+		/// Performs alphabetical sort
+		/// </summary>
+		private void btnSort_Click(object sender, EventArgs e)
+		{
+			//sort the list by name values, then update the sort order
+			List<LookupValue> list = ((IEnumerable<LookupValue>)lstValues.DataSource)?.OrderBy(lv => lv.Name.ToLower()).ToList();
+			foreach (LookupValue item in list)
+			{
+				item.SortOrder = list.IndexOf(item);
+				biz.Save(item);
+			}
+			lstValues.DataSource = list;
+		}
+	}
 }
